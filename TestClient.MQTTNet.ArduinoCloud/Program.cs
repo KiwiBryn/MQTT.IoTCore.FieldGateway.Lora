8e93aeaa3f85f9dfa68c646d7aaa1d3ac07e47ab
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2019-11-10 14:09:11 +1300

    Wound back MQTTNet Nuget upgrade and added start of Arduino integration.

---
+++ TestClient.MQTTNet.ArduinoCloud/Program.cs
@@ -0,0 +1,153 @@
+﻿/*
+    Copyright ® 2019 April devMobile Software, All Rights Reserved
+ 
+    MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE
+
+	 A quick and dirty test client to explore how Adafruit.IO MQTT connectivity works
+ */
+namespace TestClient.MQTTNet.ArduinoCloud
+{
+   using System;
+   using System.Diagnostics;
+   using System.Threading;
+   using System.Threading.Tasks;
+
+   using MQTTnet;
+   using MQTTnet.Client;
+
+   class Program
+   {
+      private static IMqttClient mqttClient = null;
+      private static IMqttClientOptions mqttOptions = null;
+      private static string server;
+      private static string username;
+      private static string password;
+      private static string clientId;
+      private static string commandTopic;
+      private static string groupname;
+      private static string feedname;
+
+      static void Main(string[] args)
+      {
+         MqttFactory factory = new MqttFactory();
+         mqttClient = factory.CreateMqttClient();
+
+         /*
+         if ((args.Length != 5) && (args.Length != 6))
+         {
+            Console.WriteLine("[MQTT Server] [UserName] [Password] [ClientID] [CommandTopic] [GroupName] [FeedName]");
+            Console.WriteLine("[MQTT Server] [UserName] [Password] [ClientID] [CommandTopic] [FeedName]");
+            Console.WriteLine("Press <enter> to exit");
+            Console.ReadLine();
+            return;
+         }
+         */
+
+         server = "mqtts-sa.iot.arduino.cc";//args[0];
+         username = "BrynHLewis";//args[1];
+         password = "5q7gUCgS0G93";//args[2];
+         clientId = "LightSensor";//args[3];
+         commandTopic = "0ffbe73a-5eb0-4bf3-a87a-10f79e6cf7cf";//args[4];
+         /*
+         if (args.Length == 6)
+         {
+            feedname = args[5].ToLower();
+            Console.WriteLine($"MQTT Server:{server} Username:{username} ClientID:{clientId} CommandTopic:{commandTopic} Feedname:{feedname}");
+         }
+
+         if (args.Length == 7)
+         {
+            groupname = args[5].ToLower();
+            feedname = args[6].ToLower();
+            Console.WriteLine($"MQTT Server:{server} Username:{username} ClientID:{clientId} CommandTopic:{commandTopic} Groupname:{groupname} Feedname:{feedname}");
+         }
+         */
+
+         mqttOptions = new MqttClientOptionsBuilder()
+            .WithTcpServer(server, 8883)
+            .WithCredentials(username, password)
+            .WithClientId(clientId)
+            .WithTls()
+            .Build();
+
+         mqttClient.Disconnected += MqttClient_Disconnected;
+         mqttClient.ConnectAsync(mqttOptions).Wait();
+         mqttClient.ApplicationMessageReceived += MqttClient_ApplicationMessageReceived;
+
+         // Adafruit.IO format for topics which are called feeds
+         string topic = string.Empty;
+         /*
+         if (args.Length == 5)
+         {
+            topic = $"{username}/feeds/{feedname}";
+         }
+
+         if (args.Length == 6)
+         {
+            topic = $"{username}/feeds/{groupname}.{feedname}";
+         }
+         */
+
+         mqttClient.SubscribeAsync(commandTopic, MQTTnet.Protocol.MqttQualityOfServiceLevel.AtMostOnce).GetAwaiter().GetResult();
+
+         while (true)
+         {
+            string value = "22." + DateTime.UtcNow.Millisecond.ToString();
+            Console.WriteLine($"Topic:{topic} Value:{value}");
+
+            var message = new MqttApplicationMessageBuilder()
+               .WithTopic(topic)
+               .WithPayload(value)
+               .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+            //.WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtMostOnce)
+            .WithExactlyOnceQoS()
+            .WithRetainFlag()
+            .Build();
+
+            Console.WriteLine("PublishAsync start");
+            mqttClient.PublishAsync(message).Wait();
+            Console.WriteLine("PublishAsync finish");
+
+            Thread.Sleep(30100);
+         }
+      }
+
+      private static void MqttClient_ApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+      {
+         Console.WriteLine($"ClientId:{e.ClientId} Topic:{e.ApplicationMessage.Topic} Payload:{e.ApplicationMessage.ConvertPayloadToString()}");
+      }
+
+      private static async void MqttClient_Disconnected(object sender, MqttClientDisconnectedEventArgs e)
+      {
+         Debug.WriteLine("Disconnected");
+         await Task.Delay(TimeSpan.FromSeconds(5));
+
+         try
+         {
+            await mqttClient.ConnectAsync(mqttOptions);
+         }
+         catch (Exception ex)
+         {
+            Debug.WriteLine("Reconnect failed {0}", ex.Message);
+         }
+      }
+   }
+}
