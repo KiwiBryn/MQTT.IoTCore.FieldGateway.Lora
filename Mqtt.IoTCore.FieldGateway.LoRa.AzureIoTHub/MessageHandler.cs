5a6b577b103f3113c6dbd780649101f793fccd66
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2019-11-23 14:55:23 +1300

    Fixed up declaration of class to be consistent with others

--- Mqtt.IoTCore.FieldGateway.LoRa.AzureIoTHub/MessageHandler.cs
+++ Mqtt.IoTCore.FieldGateway.LoRa.AzureIoTHub/MessageHandler.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Mqtt.IoTCore.FieldGateway.LoRa.AzureIoTHub
 {
-   public class MessageHandler
-   {
-   }
+    public class MessageHandler
+    {
+    }
 }
