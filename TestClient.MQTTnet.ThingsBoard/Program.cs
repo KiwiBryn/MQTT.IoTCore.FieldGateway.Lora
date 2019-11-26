3cfc6585e0a210f5338d1dde0989280df0a1cbf7
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2019-11-26 20:02:15 +1300

    Added stubbed out ThingsBoard implementation

---
+++ TestClient.MQTTnet.ThingsBoard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestClient.MQTTnet.ThingsBoard
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
