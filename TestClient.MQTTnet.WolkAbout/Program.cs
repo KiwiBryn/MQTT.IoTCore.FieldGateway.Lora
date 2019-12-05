7ce1986c204d40eb733f68a0c9663425bc0faaaf
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2019-12-05 19:13:21 +1300

    Added stubbed out Wolkabout project

---
+++ TestClient.MQTTnet.WolkAbout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestClient.MQTTnet.WolkAbout
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
