7350e090917f079eaacce2cb351a276897ffac0a
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2020-02-16 23:01:09 +1300

    First working version of MQTTNet device client for AllTHinksTalk MQTT API. Next step see how a Gateway client works.

--- TestClient.MQTTnet.AllThingsTalk/Program.cs
+++ TestClient.MQTTnet.AllThingsTalk/Program.cs
@@ -24,16 +24,111 @@
 	 A quick and dirty test client to explore how AllThingsTalk MQTT connectivity with 
 	 the MQTTnet library works
 */
-
 namespace devmobile.Mqtt.TestClient.AllThingsTalk
 {
 	using System;
+	using System.Diagnostics;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	using MQTTnet;
+	using MQTTnet.Client;
+	using MQTTnet.Client.Disconnecting;
+	using MQTTnet.Client.Options;
+
+	using Newtonsoft.Json;
+	using Newtonsoft.Json.Linq;
 
 	class Program
-   {
-      static void Main(string[] args)
-      {
-         Console.WriteLine("Hello World!");
-      }
-   }
+	{
+		private static IMqttClient mqttClient = null;
+		private static IMqttClientOptions mqttOptions = null;
+		private static string server;
+		private static string username;
+		private static string deviceID;
+
+		static void Main(string[] args)
+		{
+			MqttFactory factory = new MqttFactory();
+			mqttClient = factory.CreateMqttClient();
+
+			if ((args.Length != 3))
+			{
+				Console.WriteLine("[MQTT Server] [UserName] [ClientID]");
+				Console.WriteLine("Press <enter> to exit");
+				Console.ReadLine();
+				return;
+			}
+
+			server = args[0];
+			username = args[1];
+			deviceID = args[2];
+
+			Console.WriteLine($"MQTT Server:{server} ClientID:{deviceID}");
+
+			// AllThingsTalk formatted device state update topic
+			string topicD2C = $"device/{deviceID}/state";
+
+			mqttOptions = new MqttClientOptionsBuilder()
+				.WithTcpServer(server)
+				.WithCredentials(username, "HighlySecurePassword")
+				.WithClientId(deviceID)
+				.WithTls()
+				.Build();
+
+			mqttClient.UseDisconnectedHandler(new MqttClientDisconnectedHandlerDelegate(e => MqttClient_Disconnected(e)));
+			mqttClient.ConnectAsync(mqttOptions).Wait();
+
+			while (true)
+			{
+				JObject payloadJObject = new JObject();
+
+				double temperature = 22.0 + (DateTime.UtcNow.Millisecond / 1000.0);
+				double humidity = 50 + (DateTime.UtcNow.Millisecond / 100.0);
+
+				JObject temperatureJObject = new JObject
+				{
+					{ "value", temperature }
+				};
+				payloadJObject.Add("Temperature", temperatureJObject);
+
+				JObject humidityJObject = new JObject
+				{
+					{ "value", humidity }
+				};
+				payloadJObject.Add("Humidity", humidityJObject);
+
+				string payload = JsonConvert.SerializeObject(payloadJObject);
+				Console.WriteLine($"Topic:{topicD2C} Payload:{payload}");
+
+				var message = new MqttApplicationMessageBuilder()
+					.WithTopic(topicD2C)
+					.WithPayload(payload)
+					.WithAtMostOnceQoS()
+//					.WithAtLeastOnceQoS()
+					.Build();
+
+				Console.WriteLine("PublishAsync start");
+				mqttClient.PublishAsync(message).Wait();
+				Console.WriteLine("PublishAsync finish");
+
+				Thread.Sleep(15100);
+			}
+		}
+
+		private static async void MqttClient_Disconnected(MqttClientDisconnectedEventArgs e)
+		{
+			Debug.WriteLine("Disconnected");
+			await Task.Delay(TimeSpan.FromSeconds(5));
+
+			try
+			{
+				await mqttClient.ConnectAsync(mqttOptions);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Reconnect failed {0}", ex.Message);
+			}
+		}
+	}
 }
